39490759cbca68ecbfe04c31c97265f6a454953e
beetlejuice96 <dilanbernbae043@gmail.com>
2024-06-29 20:15:04 -0300

    setup middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,15 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 // import { config as appConfig } from '@/config';
 // import { AuthStrategy } from '@/lib/auth/strategy';
-import { supabaseMiddleware } from '@/lib/auth/supabase/middleware';
+import { supabaseMiddleware } from "@/lib/auth/supabase/middleware";
 
 export async function middleware(req: NextRequest): Promise<NextResponse> {
   let res: NextResponse;
 
   // if (appConfig.auth.strategy === AuthStrategy.SUPABASE) {
-    res = await supabaseMiddleware(req);
+  res = await supabaseMiddleware(req);
   // } else {
   //   res = NextResponse.next({ headers: req.headers });
   // }
@@ -17,4 +17,5 @@ export async function middleware(req: NextRequest): Promise<NextResponse> {
   return res;
 }
 
-export const config = { matcher: ['/auth/:path*', '/dashboard/:path*'] };
+//TODO: averiguar bien como configurar esto.
+export const config = { matcher: ["/"] };
